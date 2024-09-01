87dbc46ab15cc174bb70aae640d11601e47a3835
lerry <johnlerryramossamson@gmail.com>
2024-09-01 17:48:50 +0800

    Multer configuration added to allow user file uploads

--- server/backend/utils/multerConfig.js
+++ server/backend/utils/multerConfig.js
@@ -1,14 +1,14 @@
 import multer from 'multer';
 import { mkdir } from 'fs/promises'
-import { getDir, getFileExt } from './fileDir.js';
-import { isDirExists } from './fileDir';
+import { getDir, getFileExt, isDirExists } from './fileDir.js';
 
 export const upload = async (uploadsFolder) => {
-    const exists = await isDirExists(uploadsFolder);
-    if(!exists) await mkdir(uploadsFolder);
+    const uploadRootPath = getDir(`uploads/${uploadsFolder}`);
+    const exists = await isDirExists(uploadRootPath);
+    if(!exists) await mkdir(uploadRootPath);
     const storage = multer.diskStorage({
         destination: (req, file, cb) => {
-            cb(null, getDir(`uploads/${uploadsFolder}`));  // Assuming __dirname is /backend
+            cb(null, uploadRootPath);  // Assuming __dirname is /backend
         },
         filename: (req, file, cb) => {
             cb(null, Date.now() + getFileExt(file.originalname));  // Example for setting a unique filename
