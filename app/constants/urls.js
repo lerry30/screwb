7cf8b8d8c72ef206d796ec9b2db48caf5a98482f
lerry <johnlerryramossamson@gmail.com>
2024-09-01 17:39:42 +0800

    Registered all fetch urls

--- app/constants/urls.js
+++ app/constants/urls.js
@@ -1,4 +1,4 @@
-export const serverDomain = 'http://192.168.0.111:3000';
+export const serverDomain = 'http://192.168.0.107:3000';
 
 export const urls = {
     signup: `${serverDomain}/api/users`,
@@ -6,4 +6,8 @@ export const urls = {
     profileimage: `${serverDomain}/api/users/profileimage`,
     updateprofile: `${serverDomain}/api/users/profile`,
     createpost: `${serverDomain}/api/posts/create`,
+    getvideos: `${serverDomain}/api/videos`,
+    feedback: `${serverDomain}/feedback`,
+    reactions: `${serverDomain}/feedback/user`,
+    postreactions: `${serverDomain}/feedback/post`,
 }
