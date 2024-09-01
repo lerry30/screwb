b04b2b0adb831b618f5a696fab2dceed2d9114c8
lerry <johnlerryramossamson@gmail.com>
2024-09-01 17:44:14 +0800

    Make this function more neat and compact also to send token for verifications as well as adding component property to dynamically set the method of the request

--- app/utils/send.js
+++ app/utils/send.js
@@ -10,12 +10,12 @@ const saveToken = async (payload) => {
     }
 }
 
-export const sendJSON = async (urlPath, payload) => {
+export const sendJSON = async (urlPath, payload, method="POST") => {
     const token = await getFromLocal('token');
     payload['token'] = token;
 
     const res = await fetch(urlPath, {
-        method: 'POST',
+        method: method,
         headers: {
             'Content-Type': 'application/json',
         },
@@ -33,12 +33,12 @@ export const sendJSON = async (urlPath, payload) => {
     return resPayload;
 }
 
-export const sendForm = async (urlPath, form) => {
+export const sendForm = async (urlPath, form, method="POST") => {
     const token = await getFromLocal('token');
     form.append('token', token);
 
     const res = await fetch(urlPath, {
-        method: 'POST',
+        method: method,
         body: form
     });
 
@@ -61,48 +61,5 @@ export const getData = async (urlPath) => {
         throw new Error(message, { cause: { response: res, payload } });
     }
 
-    return payload;
-}
-
-export const deleteWithJSON = async (urlPath, payload) => {
-    const token = await getFromLocal('token');
-    payload['token'] = token;
-
-    const res = await fetch(urlPath, {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(payload)
-    });
-
-    const resPayload = await res.json();
-    // you might get an error saying new Error is error when your urlpath is invalid or wrong
-    if(!res?.ok) {
-        const message = resPayload?.message || 'Failed to fetch';
-        throw new Error(message, { cause: { status: res.status, payload: resPayload } });
-    }
-
-    saveToken(resPayload);
-    return resPayload;
-}
-
-export const sendFormUpdate = async (urlPath, form) => {
-    const token = await getFromLocal('token');
-    form.append('token', token);
-
-    const res = await fetch(urlPath, {
-        method: 'PUT',
-        body: form
-    });
-
-    const payload = await res.json();
-    if(!res?.ok) {
-        const message = payload?.message || 'Failed to fetch';
-        console.log('Error ', { message });
-        throw new Error(message, { cause: { response: res, payload } });
-    }
-
-    saveToken(payload);
     return payload;
 }
