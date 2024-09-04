c829417c071951a11ceeafc4605a62ffa5f4fee5
lerry <johnlerryramossamson@gmail.com>
2024-09-04 19:10:31 +0800

    New route added to search for posts

--- server/backend/routes/videoRoutes.js
+++ server/backend/routes/videoRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 
 import { 
-    getVideos
+    getVideos,
+    searchVideos,
 } from '../controllers/videoController.js';
 
 const router = express.Router();
 router.get('/', getVideos);
+router.get('/search', searchVideos);
 
 export default router;
