43b79411c47a123097f58b70f43f0392f36c394c
lerry <johnlerryramossamson@gmail.com>
2024-09-04 19:00:54 +0800

    Logo added and make it more engaging

--- app/app/index.jsx
+++ app/app/index.jsx
@@ -1,6 +1,6 @@
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, ScrollView, Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-//import { Video, ResizeMode } from 'expo-av'
+import { Video, ResizeMode } from 'expo-av'
 import { useRouter } from 'expo-router';
 import { getFromLocal } from '@/utils/localStorage';
 import { useLayoutEffect } from 'react';
@@ -9,6 +9,7 @@ import { zPosts } from '@/store/posts';
 
 import Background from '@/components/Background';
 import BackgroundVideo from '@/assets/videos/background.mp4';
+import AppLogo from '@/assets/logo.png';
 import CustomButton from '@/components/CustomButton';
 
 const HomePage = () => {
@@ -39,19 +40,20 @@ const HomePage = () => {
         <SafeAreaView>
             <ScrollView>
                 <View className="relative flex-1 size-screen min-h-screen">
-                    {/*<Video 
+                    <Video 
                         source={BackgroundVideo}
                         className="absolute flex-1 size-screen top-0 left-0 bottom-0 right-0"
                         resizeMode={ResizeMode.COVER}
                         shouldPlay
                         isLooping
                         isMuted
-                    />*/}
+                    />
 
                     <Background style={{position: 'absolute', top: 0, bottom: 0, left: 0, right: 0}}>
-                        <View className="flex-1 size-screen p-8 flex justify-center items-center">
-                            <Text className="text-secondary-200 font-pthin text-lg text-center">Screwb is a small and lightweight app that lets you watch short videos within a limited time, helping you break your scrolling addiction. It provides an incredible experience while reminding you to take breaks. Once you reach the maximum scroll limit, the app will automatically stop.</Text>
-                            <CustomButton title="Get Started" onPress={() => router.push('/(tabs)/home')} contClassName="w-full mt-4" />
+                        <View className="flex-1 size-screen p-8 flex justify-center items-center space-y-[40px]">
+                            <Image className="h-[70px]" source={AppLogo} resizeMode="contain" />
+                            <Text className="text-white font-pthin text-lg text-center">Screwb is a small and lightweight app that lets you watch short videos within a limited time, helping you break your scrolling addiction. It provides an incredible experience while reminding you to take breaks. Once you reach the maximum scroll limit, the app will automatically stop.</Text>
+                            <CustomButton title="Get Started" onPress={() => router.push('/(tabs)/home')} contClassName="w-full mt-[40px]" />
                         </View>
                     </Background>
                 </View>
